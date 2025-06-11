122c427b4c9e37c08923f5af2ff39a89a769f350
Caleb <hallockdesigns.21@gmail.com>
2025-06-10 23:55:34 -0500

    Completed Assignment

--- functionality.js
+++ functionality.js
@@ -1,19 +1,49 @@
 //Pull objects in to manipulate
 const addItem = document.getElementById("add-item");
-const newItem = document.getElementById("new-item");
 const shoppingList = document.getElementById("shopping-list");
+const newItem = document.getElementById("new-item");
 
-function addingItems(newItem) {
-    shoppingList.innerHTML += `<li id="item 1">${newItem}</li>`
+function addingItems() {
+    let ItemValue = newItem.value;
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    let editButton = document.createElement('button');
+    let confirmButton = document.createElement('button');
+
+    li.textContent = ItemValue;
+    shoppingList.appendChild(li);
     
-}
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-button");
+    li.appendChild(deleteButton);
+    newItem.value = "";
+    
+        editButton.textContent = "Edit";
+        editButton.classList.add("edit-button");
+        li.appendChild(editButton);
+
+    editButton.addEventListener("click",() =>{
+        li.innerHTML = `<input value="${ItemValue}" id="updated-value"></input>`
+        confirmButton.textContent = "Confirm"
+        confirmButton.classList.add("confirm-button");
+        li.appendChild(confirmButton);
+    })
+
+    confirmButton.addEventListener("click",() => {
+        console.log("confirm Changes");
+        let updatedValue = document.getElementById("updated-value").value;
+        li.textContent = updatedValue;
+        li.appendChild(deleteButton);
+        li.appendChild(editButton);
+    })
+
+    deleteButton.addEventListener("click", () => {
+        console.log("item removed!")
+        li.remove();
+    });
+};
 
 addItem.addEventListener("click", () => {
-    const li = createElement("li");
-    let newItemValue = newItem.text
-    // const listItem = document.createElement("li");
-    console.log(newItemValue);
-    addingItems(newItemValue);
-    // shoppingList.listItem.appendChild(newItem);
-    
-})
+    console.log('item added!')
+    addingItems();
+});
